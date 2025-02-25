5e3afbe35fe40d26a8cd06cb47f44c1a6c546a3f
J <2389449@cognizant.com>
2025-02-25 21:37:58 +0530

    project structure

---
+++ assignment-module/src/main/java/com/cts/assignmentmodule/AssignmentModuleApplication.java
@@ -0,0 +1,13 @@
+package com.cts.assignmentmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentModuleApplication.class, args);
+	}
+
+}
---
+++ course-module/src/test/java/com/cts/coursemodule/CourseModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.coursemodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
